028584048ac47efaf123a68b8d1972132695eedb
Ivan Muliarchyk <ya-gans@mail.ru>
2017-09-05 12:36:53 +0300

    Added backend.save to setup module

--- js/render.js
+++ js/render.js
@@ -9,8 +9,30 @@
   var wizardCoat = wizardSetup.querySelector('.wizard-coat');
   var wizardEyes = wizardSetup.querySelector('.wizard-eyes');
   var fireball = setup.querySelector('.setup-fireball-wrap');
-  var WIZARD_PARAMS = window.data.WIZARD_PARAMS;
-
+  var WIZARD_PARAMS = {
+    coatColors: [
+      'rgb(101, 137, 164)',
+      'rgb(241, 43, 107)',
+      'rgb(146, 100, 161)',
+      'rgb(56, 159, 117)',
+      'rgb(215, 210, 55)',
+      'rgb(0, 0, 0)'
+    ],
+    eyesColors: [
+      'black',
+      'red',
+      'blue',
+      'yellow',
+      'green'
+    ],
+    fireballColors: [
+      '#ee4830',
+      '#30a8ee',
+      '#5ce6c0',
+      '#e848d5',
+      '#e6e848'
+    ]
+  };
 
   function renderWizard(wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
